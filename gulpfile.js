a7c05adf9d6098a0bad9e1f1ad7d447cbab84123
Miguel Camberos <miguel.camberos@centricity.com.au>
2016-09-14 14:02:13 +1000

    Added basic structure, .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Include gulp
+var gulp    = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    gutil = require('gulp-util');
 
-var gulp = require('gulp');
-
-gulp.task('default', function () {
-    // place code for your default task here
+// Define tasks
+gulp.task('minify', function() {
+  console.log('Minifying HTML');
+  return gulp.src('public_html/src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('public_html/build/'));
 });
+
+
+gulp.task('default', function() {
+    // Code for the default task
+    gulp.start('minify');
+    // return gutil.log('Gulp is running!');
+});
+
